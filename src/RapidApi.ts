760e959c2178fce0e9bf5e193ada86a3fc6224e1
Angel S. Moreno <angelxmoreno@gmail.com>
2024-02-02 17:13:52 -0500

    feat: created RapiApi class

---
+++ src/RapidApi.ts
@@ -0,0 +1,46 @@
+import { AxiosInstance, CreateAxiosDefaults } from 'axios';
+import Keyv from 'keyv';
+import { addAxiosDateTransformer, createAxiosDateTransformer } from 'axios-date-transformer';
+
+interface RapidApiParams {
+    rapidApiKey: string;
+    rapidApiHost: string;
+    baseUrl: string;
+    axiosConfig?: CreateAxiosDefaults;
+    axiosInstance?: AxiosInstance;
+    cache?: Keyv;
+}
+
+export class RapidApi {
+    rapidApiKey: string;
+    rapidApiHost: string;
+    baseUrl: string;
+    axiosInstance: AxiosInstance;
+    cache: Keyv;
+
+
+    constructor({ rapidApiKey, rapidApiHost, baseUrl, axiosInstance, axiosConfig, cache }: RapidApiParams) {
+        this.rapidApiKey = rapidApiKey;
+        this.rapidApiHost = rapidApiHost;
+        this.baseUrl = baseUrl;
+        this.axiosInstance = this.configureAxiosInstance(axiosInstance, axiosConfig);
+        this.cache = this.configureCache(cache);
+    }
+
+    protected configureAxiosInstance(axiosInstance?: AxiosInstance, axiosConfig?: CreateAxiosDefaults): AxiosInstance {
+        const instance = !!axiosInstance
+            ? addAxiosDateTransformer(axiosInstance)
+            : createAxiosDateTransformer(axiosConfig);
+
+        instance.defaults.baseURL = this.baseUrl;
+        instance.defaults.headers.common['X-RapidAPI-Host'] = this.rapidApiHost;
+        instance.defaults.headers.common['X-RapidAPI-Key'] = this.rapidApiKey;
+        instance.defaults.headers.common['Content-Type'] = 'application/json';
+
+        return instance;
+    }
+
+    protected configureCache(cache?: Keyv): Keyv {
+        return cache || new Keyv();
+    }
+}
